278f3cfc0ee3babed99b06c14881f789cc457615
Sandeepdev <palsandeep894@hotmail.com>
2026-09-19 11:22:30 +0530

    add vercel.json for deployment

--- Backend/src/utils/commonFunctions.ts
+++ Backend/src/utils/commonFunctions.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { SALT_ROUNDS } from "../constants";
+import { SALT_ROUNDS } from "../constants.js";
 
 const hashPassword = async (password: string) => {
   return await bcrypt.hash(password, SALT_ROUNDS);
